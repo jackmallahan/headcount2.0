f8edd5044c22f8bd281d4867625472b8cc112e6e
Jack Mallahan <jackmallahan@gmail.com>
2017-08-19 14:14:51 -0600

    added comparison stats to ComparisonCard component

--- src/components/ComparisonCard.js
+++ src/components/ComparisonCard.js
@@ -1,14 +1,34 @@
-import React from "react";
-import "../styles/DistrictRepository.css";
+import React from 'react';
+import '../styles/DistrictRepository.css';
+import { DistrictRepository } from '../helpers/DistrictRepository';
+import KinderData from '../../data/kindergartners_in_full_day_program';
 
-const ComparisonCard = props => {
-  return (
-    <div className="card comparison-card">
-      <h3>Card 1 Average</h3>
-      <h3>Ratio</h3>
-      <h3>Card 2 Average</h3>
-    </div>
-  );
+const ComparisonCard = ({
+	compareAverages,
+	fullData,
+	findAverage,
+	selected
+}) => {
+	const district = new DistrictRepository(KinderData);
+	const card1Average = district.findAverage(selected[0]);
+	const card2Average = district.findAverage(selected[1]);
+	const comparedRatio = district.compareDistrictAverages(
+		selected[0],
+		selected[1]
+	);
+	return (
+		<div className="card comparison-card">
+			<h3>
+				{selected[0]} Average: {card1Average}
+			</h3>
+			<h3>
+				Ratio: {comparedRatio.compared}
+			</h3>
+			<h3>
+				{selected[1]} Average: {card2Average}
+			</h3>
+		</div>
+	);
 };
 
 export default ComparisonCard;
--- src/components/DistrictContainer.js
+++ src/components/DistrictContainer.js
@@ -1,39 +1,47 @@
-import React from "react";
-import DistrictCard from "./DistrictCard";
-import ComparisonCard from "./ComparisonCard";
-import "../styles/DistrictRepository.css";
+import React from 'react';
+import DistrictCard from './DistrictCard';
+import ComparisonCard from './ComparisonCard';
+import '../styles/DistrictRepository.css';
 
 const DistrictContainer = props => {
-  return (
-    <div className="district-container">
-      <div className="selected-cards">
-        {props.selected.length > 0
-          ? props.selected.map((card, index) =>
-              <DistrictCard
-                location={card}
-                data={props.fullData[card]}
-                getData={props.getData}
-                key={Math.random()}
-                findDistrict={props.findDistrict}
-                className="card selected-district-card"
-                order={index * 2}
-              />
-            )
-          : <div>Select two districts to compare data</div>}
-        {props.selected.length === 2 ? <ComparisonCard /> : null}
-      </div>
-      {props.foundData.map(place =>
-        <DistrictCard
-          location={place}
-          data={props.fullData[place]}
-          getData={props.getData}
-          key={Math.random()}
-          findDistrict={props.findDistrict}
-          className="card"
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className="district-container">
+			<div className="selected-cards">
+				{props.selected.length > 0
+					? props.selected.map((card, index) =>
+							<DistrictCard
+								location={card}
+								data={props.fullData[card]}
+								getData={props.getData}
+								key={Math.random()}
+								findDistrict={props.findDistrict}
+								className="card selected-district-card"
+								order={index * 2}
+								findAverage={props.findAverage}
+							/>
+						)
+					: <div>Select two districts to compare data</div>}
+				{props.selected.length === 2
+					? <ComparisonCard
+							fullData={props.fullData}
+							findAverage={props.findAverage}
+							compareAverages={props.compareAverages}
+							selected={props.selected}
+						/>
+					: null}
+			</div>
+			{props.foundData.map(place =>
+				<DistrictCard
+					location={place}
+					data={props.fullData[place]}
+					getData={props.getData}
+					key={Math.random()}
+					findDistrict={props.findDistrict}
+					className="card"
+				/>
+			)}
+		</div>
+	);
 };
 
 export default DistrictContainer;
