5832b1c561f109763bdbe96d48900ef609adf689
Tyler Hevia <tylerhevia@gmail.com>
2017-08-21 02:21:10 -0600

    organized code

--- src/components/ComparisonCard.js
+++ src/components/ComparisonCard.js
@@ -1,48 +1,49 @@
-import React from 'react';
-import '../styles/styles.css';
-import { DistrictRepository } from '../helpers/DistrictRepository';
-import KinderData from '../../data/kindergartners_in_full_day_program';
-import PropTypes from 'prop-types';
+import React from "react";
+import "../styles/styles.css";
+import { DistrictRepository } from "../helpers/DistrictRepository";
+import KinderData from "../../data/kindergartners_in_full_day_program";
+import PropTypes from "prop-types";
 
 const ComparisonCard = ({
-	compareAverages,
-	fullData,
-	findAverage,
-	selected
+  compareAverages,
+  fullData,
+  findAverage,
+  selected
 }) => {
-	const district = new DistrictRepository(KinderData);
-	const card1Average = district.findAverage(selected[0]);
-	const card2Average = district.findAverage(selected[1]);
-	const comparedRatio = district.compareDistrictAverages(
-		selected[0],
-		selected[1]
-	);
-	return (
-		<article className="card comparison-card">
-			<h3>
-				{selected[0]}
-				<span className="compare-data">
-					{card1Average}
-				</span>
-			</h3>
-			<h3>
-				Ratio<span className="compare-data">{comparedRatio.compared}</span>
-			</h3>
-			<h3>
-				{selected[1]}
-				<span className="compare-data">
-					{card2Average}
-				</span>
-			</h3>
-		</article>
-	);
+  const district = new DistrictRepository(KinderData);
+  const card1Average = district.findAverage(selected[0]);
+  const card2Average = district.findAverage(selected[1]);
+  const comparedRatio = district.compareDistrictAverages(
+    selected[0],
+    selected[1]
+  );
+
+  return (
+    <article className="card comparison-card">
+      <h3>
+        {selected[0]}
+        <span className="compare-data">
+          {card1Average}
+        </span>
+      </h3>
+      <h3>
+        Ratio<span className="compare-data">{comparedRatio.compared}</span>
+      </h3>
+      <h3>
+        {selected[1]}
+        <span className="compare-data">
+          {card2Average}
+        </span>
+      </h3>
+    </article>
+  );
 };
 
 ComparisonCard.propTypes = {
-	compareAverages: PropTypes.func,
-	fullData: PropTypes.object,
-	findAverage: PropTypes.func,
-	selected: PropTypes.arrayOf(PropTypes.string)
+  compareAverages: PropTypes.func,
+  fullData: PropTypes.object,
+  findAverage: PropTypes.func,
+  selected: PropTypes.arrayOf(PropTypes.string)
 };
 
 export default ComparisonCard;
--- src/components/DistrictCard.js
+++ src/components/DistrictCard.js
@@ -1,75 +1,76 @@
-import React from 'react';
-import '../styles/styles.css';
-import PropTypes from 'prop-types';
-import { DistrictRepository } from '../helpers/DistrictRepository';
-import KinderData from '../../data/kindergartners_in_full_day_program';
+import React from "react";
+import "../styles/styles.css";
+import PropTypes from "prop-types";
+import { DistrictRepository } from "../helpers/DistrictRepository";
+import KinderData from "../../data/kindergartners_in_full_day_program";
 
 const DistrictCard = ({
-	location,
-	data,
-	getData,
-	findDistrict,
-	className,
-	order
+  location,
+  data,
+  getData,
+  findDistrict,
+  className,
+  order
 }) => {
-	const stylez = {
-		color: 'rgb(255, 255, 250)'
-	};
-	const underFive = {
-		color: 'red'
-	};
-	const cardOrder = {
-		order: order
-	};
-	const district = new DistrictRepository(KinderData);
-	return (
-		<article
-			className={className}
-			onClick={e => findDistrict(e, location)}
-			style={cardOrder}
-		>
-			<h3>
-				{location}
-			</h3>
-			<h5>
-				Average: {district.findAverage(location)}
-			</h5>
-			<ul className="card-list">
-				{data.sort((a, b) => a.TimeFrame - b.TimeFrame).map((year, i) => {
-					year.Data =
-						typeof year.Data === 'number'
-							? parseFloat(year.Data.toFixed(3))
-							: 0;
+  const stylez = {
+    color: "rgb(255, 255, 250)"
+  };
+  const underFive = {
+    color: "red"
+  };
+  const cardOrder = {
+    order: order
+  };
+  const district = new DistrictRepository(KinderData);
 
-					return (
-						<li key={i}>
-							<span
-								className="card-year"
-								style={year.Data < 0.5 ? underFive : stylez}
-							>
-								{year.TimeFrame}:{' '}
-							</span>
-							<span
-								className="year-data"
-								style={year.Data < 0.5 ? underFive : stylez}
-							>
-								{year.Data}
-							</span>
-						</li>
-					);
-				})}
-			</ul>
-		</article>
-	);
+  return (
+    <article
+      className={className}
+      onClick={e => findDistrict(e, location)}
+      style={cardOrder}
+    >
+      <h3>
+        {location}
+      </h3>
+      <h5>
+        Average: {district.findAverage(location)}
+      </h5>
+      <ul className="card-list">
+        {data.sort((a, b) => a.TimeFrame - b.TimeFrame).map((year, i) => {
+          year.Data =
+            typeof year.Data === "number"
+              ? parseFloat(year.Data.toFixed(3))
+              : 0;
+
+          return (
+            <li key={i}>
+              <span
+                className="card-year"
+                style={year.Data < 0.5 ? underFive : stylez}
+              >
+                {year.TimeFrame}:{" "}
+              </span>
+              <span
+                className="year-data"
+                style={year.Data < 0.5 ? underFive : stylez}
+              >
+                {year.Data}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </article>
+  );
 };
 
 DistrictCard.propTypes = {
-	location: PropTypes.string.isRequired,
-	data: PropTypes.arrayOf(PropTypes.object),
-	getData: PropTypes.func,
-	findDistrict: PropTypes.func,
-	className: PropTypes.string,
-	order: PropTypes.number
+  location: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(PropTypes.object),
+  getData: PropTypes.func,
+  findDistrict: PropTypes.func,
+  className: PropTypes.string,
+  order: PropTypes.number
 };
 
 export default DistrictCard;
